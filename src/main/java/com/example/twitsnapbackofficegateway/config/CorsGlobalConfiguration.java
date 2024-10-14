31926fbffcd3883105187ebee2970563c6784569
Guido Menendez <guidomenendez0@gmail.com>
2024-10-14 17:40:21 -0300

    Cors fix

---
+++ src/main/java/com/example/twitsnapbackofficegateway/config/CorsGlobalConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.twitsnapbackofficegateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsGlobalConfiguration{
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("https://twitsnap-backoffice-g8xi.onrender.com");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
